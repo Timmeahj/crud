7a51943475bd22d6f90345bc5bfea05b4b9d6c64
Jörg von Dzerzawa <jvondzerza@googlemail.com>
2021-12-08 12:04:51 +0100

    Delete edit.php

--- View/edit.php
+++
@@ -1,9 +0,0 @@
-<form method="post" action="">
-    <input name="id" type="number" placeholder="">
-    <input name="name" type="text" placeholder="">
-    <input name="email" type="text" placeholder="">
-    <select>
-        <?php $display->classIdOptions($connection, $table) ?>
-    </select>
-    <input name="teacher_name" type="text" placeholder="">
-</form>
